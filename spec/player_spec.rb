6c1627285bebbfe6c947547cc5b2a0bd86b7f11a
Jose <jcopoviking@outlook.com>
2016-07-20 16:20:10 +0100

    Ch23 - Extracts logic to model using Player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,10 @@
+require 'player'
+
+describe Player do
+subject(:bob) { Player.new( "Bob" ) }
+  describe "#name" do
+    it "returns the name" do
+    expect(bob.name).to eq "Bob"
+    end
+  end
+end
