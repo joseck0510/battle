65723d21e2858e8ae4964f2cb01343768e9e9802
Harry Jaggs <harryjaggs92@gmail.com>
2016-07-22 12:44:56 +0100

    step 28 - multiplayer - tests pass

--- spec/features/switch_turns_spec.rb
+++ spec/features/switch_turns_spec.rb
@@ -6,9 +6,15 @@ feature 'switches turns' do
 
   scenario 'after player 1 attacks it switches players' do
     sign_in_and_play
-    click_button 'attack'
-    click_button 'Switch turns'
+    attack_and_switch_turns
     expect(page).to have_content "Noby's turn"
     expect(page).not_to have_content "Albie's turn"
+  end
+  scenario 'after player 2 attacks it switches players' do
+    sign_in_and_play
+    attack_and_switch_turns
+    attack_and_switch_turns
+    expect(page).to have_content "Albie's turn"
+    expect(page).not_to have_content "Noby's turn"
   end
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -4,3 +4,8 @@ def sign_in_and_play
   fill_in :Name2, with: 'Noby'
   click_button "submit"
 end
+
+def attack_and_switch_turns
+  click_button 'attack'
+  click_button 'Switch turns'
+end
